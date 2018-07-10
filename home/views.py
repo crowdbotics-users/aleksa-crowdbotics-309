8c5f61689df509d29ffaf97dc13b15d7db136580
Saurav <sp.gharti@gmail.com>
2018-07-10 16:17:41 +0000

    add package django-shop

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-shop', 'url': 'http://pypi.python.org/pypi/django-shop/0.11.3'},
     ]
     context = {
         'customtext': CustomText.objects.first(),
